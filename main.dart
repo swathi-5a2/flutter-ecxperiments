7bb0ee909d3065c1c32ce30733b196a481b6ddb8
swathi-5a2 <23221a05a2@bvcgroup.in>
2025-11-07 14:10:58 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
-  @override Widget build(BuildContext c) => MaterialApp(home: ResponsiveHome());
-}
-class ResponsiveHome extends StatelessWidget {
-  @override Widget build(BuildContext c) {
-    double w = MediaQuery.of(c).size.width;
-    return Scaffold(
-      body: Center( 
-        child: Container(
-          width: w * 0.6,
-          height: w * 0.4,
-          color: w < 400 ? Colors.orange : Colors.blue,
-          child: container(child:center('{width :$w.toInt}')),
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            color: Colors.blue,
+            child: const Center(
+              child: Text(
+                "Hello",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
